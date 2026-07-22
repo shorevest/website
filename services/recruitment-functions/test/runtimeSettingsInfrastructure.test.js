9cce3a081705de0d2e11d7aafa11a55c82c457ec
nicolalalalalalalalala <ineedawatch2@gmail.com>
2026-07-23 00:50:55 +0800

    Test recruitment runtime settings defaults

---
+++ services/recruitment-functions/test/runtimeSettingsInfrastructure.test.js
@@ -0,0 +1,42 @@
+'use strict';
+
+const test = require('node:test');
+const assert = require('node:assert');
+const fs = require('fs');
+const path = require('path');
+
+const template = fs.readFileSync(
+  path.resolve(__dirname, '../../../infra/recruitment/runtime-settings.bicep'),
+  'utf8'
+);
+
+test('all external recruitment capabilities default disabled', () => {
+  assert.ok(template.includes('param apiEnabled bool = false'));
+  assert.ok(template.includes('param outboxDeliveryEnabled bool = false'));
+  assert.ok(template.includes('param candidateAcknowledgementEnabled bool = false'));
+  assert.ok(template.includes('param candidateAcknowledgementTemplateApproved bool = false'));
+  assert.ok(template.includes('param hrAccessEnabled bool = false'));
+  assert.ok(template.includes('param platformAuthenticationEnabled bool = false'));
+});
+
+test('runtime settings use managed identity and no storage connection string', () => {
+  assert.ok(template.includes("AzureWebJobsStorage__credential: 'managedidentity'"));
+  assert.ok(template.includes('AzureWebJobsStorage__clientId: managedIdentity.properties.clientId'));
+  assert.ok(template.includes('RECRUITMENT_MANAGED_IDENTITY_CLIENT_ID: managedIdentity.properties.clientId'));
+  assert.ok(!template.includes('AccountKey='));
+  assert.ok(!template.includes('SharedAccessSignature='));
+});
+
+test('runtime settings centralize SharePoint, acknowledgement and HR gates', () => {
+  for (const setting of [
+    'RECRUITMENT_SHAREPOINT_SITE_ID',
+    'RECRUITMENT_APPLICATIONS_LIST_ID',
+    'RECRUITMENT_FILES_LIST_ID',
+    'RECRUITMENT_CANDIDATE_ACK_TEMPLATE_APPROVED',
+    'RECRUITMENT_HR_ACCESS_ENABLED',
+    'RECRUITMENT_PLATFORM_AUTH_ENABLED',
+    'RECRUITMENT_HR_REQUIRED_ROLE'
+  ]) {
+    assert.ok(template.includes(setting));
+  }
+});
